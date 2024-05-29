1c51495454ce5e11dffb52a82c9903ea888eac6f
iven <iven840320@gmail.com>
2024-05-29 13:28:53 +0800

    Merge remote-tracking branch 'origin/TIM08'

---
+++ HaveFun_01/Controllers/TIM08Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun_01.Controllers
+{
+    public class TIM08Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
