cb193092b9a3b82fc7e17346e3c291b368389df4
Christopher <Allen71798900@gmail.com>
2024-05-29 09:56:25 +0800

    aaaaaaa06

---
+++ HaveFun_01/Controllers/PracticeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun_01.Controllers
+{
+    public class PracticeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
